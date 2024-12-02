8713a20ad52c6a4711979f2c8d09c065cded13d8
Daniel1Maymon <daniel1maymon@gmail.com>
2024-12-02 17:41:32 +0200

    changes for testing purposes

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -40,7 +40,7 @@ export default {
   methods: {
   async handleLogin() {
   try {
-    const response = await fetch("http://127.0.0.1:5000/auth/login", {
+    const response = await fetch("http://considerate-nourishment.railway.internal:5000/auth/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
