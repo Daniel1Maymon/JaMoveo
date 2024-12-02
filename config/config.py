c83b81808d8b383fdfd0bff8ef74cc51b5e4cfb7
Daniel <daniel1maymon@gmail.com>
2024-12-02 14:01:20 +0200

    Update gitignore file and uploade config file

---
+++ config/config.py
@@ -0,0 +1,17 @@
+import os
+import secrets
+
+class Config:
+    # SECRET_KEY = secrets.token_hex(32)
+    SECRET_KEY = os.getenv("SECRET_KEY", "default_secret_key")
+    
+    
+    
+    MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/defaultdb")
+    
+
+    # MONGO_URI = "mongodb+srv://<username>:<password>@<cluster-name>.mongodb.net/<DB_name>"
+
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+    SONGS_DIR = os.path.join(BASE_DIR, '../app/songs')
+    SONGS_FOLDER = os.path.abspath(SONGS_DIR)
